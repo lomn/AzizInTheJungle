9223b9eda51b2622c149ccc4b4abac3513b961c1
clement <lomn.contact@gmail.com>
2020-12-11 14:41:39 +0100

    op = + dest bestiole

--- src/Bestiole.h
+++ src/Bestiole.h
@@ -56,6 +56,7 @@ public :                                           // Forme canonique :
 
    void initCoords( int xLim, int yLim );
 
+   Bestiole & operator=(const Bestiole & b);
    friend bool operator==( const Bestiole & b1, const Bestiole & b2 );
    friend bool operator!=( const Bestiole & b1, const Bestiole & b2 ){return not(b1 == b2);};
 
