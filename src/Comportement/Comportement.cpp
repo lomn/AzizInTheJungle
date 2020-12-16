9c3b929c63ca57c85378f735f377951e0d461689
Amaury Tisseau <amaury.tisseau@gmail.com>
2020-12-16 18:19:15 +0100

    la methode calculVitesse de comportement retourne maintenant la vitesse de la bestiole, les prevoyants ne seront plus immobiles

--- src/Comportement/Comportement.cpp
+++ src/Comportement/Comportement.cpp
@@ -3,6 +3,8 @@
 
 std::array<double, 2> Comportement::calculVitesse( Bestiole &b, std::vector<Bestiole> & list) {
     std::array<double, 2> vitesse{0,0};
+    vitesse[0] = b.getVitessePolaire();
+    vitesse[1] = b.getOrientation();
     return vitesse;
 }
 
